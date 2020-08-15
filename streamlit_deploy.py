0c42a090ae2fd4c9b640272979db681ca3439aca
emagnusson88 <emag39@gmail.com>
2020-08-15 13:12:57 -0700

    changing data path

--- streamlit_deploy.py
+++ streamlit_deploy.py
@@ -8,7 +8,7 @@ st.title("UFC Prediction")
 
 st.subheader('Using past MMA fight and fighter data to predict the outcome of future bouts')
 
-predictions = st.cache(pd.read_csv)(".\data\predictions.csv")
+predictions = st.cache(pd.read_csv)("C:\Users\emag3\Documents\Code\Python\UFC-Prediction\data\predictions.csv")
 
 #st.sidebar.markdown('Raw predictions')
 st.table(predictions)
