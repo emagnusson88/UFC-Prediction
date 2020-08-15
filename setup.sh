e85da36bee834014e29436f5a4e68731d10b8a2c
emagnusson88 <emag39@gmail.com>
2020-08-15 12:38:40 -0700

    Update streamlit setup file

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [general]\n\
 email = \"emag39@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+
 " > ~/.streamlit/config.toml
