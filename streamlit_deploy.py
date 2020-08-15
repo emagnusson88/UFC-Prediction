c3349ceac98e437dfc030d3ce364ee70f06c97be
emagnusson88 <emag39@gmail.com>
2020-08-15 12:24:58 -0700

    Updating Heroku files

--- streamlit_deploy.py
+++ streamlit_deploy.py
@@ -8,8 +8,7 @@ st.title("UFC Prediction")
 
 st.subheader('Using past MMA fight and fighter data to predict the outcome of future bouts')
 
+predictions = st.cache(pd.read_csv)(".\data\predictions.csv")
 
-predictions = pd.read_csv(".\data\predictions.csv")
-
-st.sidebar.markdown('Raw predictions')
+#st.sidebar.markdown('Raw predictions')
 st.table(predictions)
