f3c1741e7afb17a5003ff3c2929a607f1b54fd25
emagnusson88 <emag39@gmail.com>
2020-08-16 17:40:29 -0700

    adding UFC logo to web app

--- streamlit_deploy.py
+++ streamlit_deploy.py
@@ -3,8 +3,13 @@ import pandas as pd
 import numpy as np
 import altair as alt
 import pydeck as pdk
+from PIL import Image
 
-st.title("UFC Prediction")
+image = Image.open("./archive/ufc_logo.png")
+
+st.image(image,use_column_width=True, output_format='PNG')
+
+st.title("UFC Fight Prediction")
 
 st.subheader('Using past MMA fight and fighter data to predict the outcome of future bouts')
 
